a5f45020b147d60ff1aa3eb8a17f08729d84e0d4
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2024-06-11 14:43:02 -0500

    Update condor_trigger.sh

--- condor_trigger.sh
+++ condor_trigger.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export USER="$(id -u -n)"
 export LOGNAME=${USER}
-export HOME=/sphenix/u/${LOGNAME}
+export HOME=/sphenix/user/${LOGNAME}
 export MYINSTALL="$HOME/install"
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n
@@ -9,5 +9,17 @@ source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
 
 events=$1
 filename=$2
-outname1=$3
-root -b -l -q "macro/Fun4All_CaloTreeGen.C($events,\"$filename\",\"$outname1\")"
+outname="CaloOutput_${filename%.*}.root"  # Strip the extension from the filename and append
+
+root -b -l -q "macros/Fun4All_Calo_Emulator.C($events, \"$filename\", \"$outname\")"
+
+
+universe                = vanilla
+executable              = /sphenix/user/patsfan753/condor_calotreegen.sh
+arguments               = 44686 $(filename) $(Cluster)
+log                     = /sphenix/user/patsfan753/logs/job.$(Cluster).$(Process).log
+output                  = /sphenix/user/patsfan753/out/job.$(Cluster).$(Process).out
+error                   = /sphenix/user/patsfan753/err/job.$(Cluster).$(Process).err
+request_memory          = 7GB
+queue filename from /sphenix/u/patsfan753/scratch/analysis/calotriggeremulator/dst_calo_run2pp-00044686.list
+
